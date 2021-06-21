e24eaf47e57efe73f9445280432a6bf95576c612
Lucas Passeri <lucaspasseri@poli.ufrj.br>
2021-06-21 19:25:14 -0300

    Starting point...

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import ReactDOM from 'react-dom';
+
+function App() {
+    return (
+        <div>
+            Bora? Bora!
+		</div>
+    );
+}
+
+const app = App();
+ReactDOM.render(app, document.querySelector(".root"));
