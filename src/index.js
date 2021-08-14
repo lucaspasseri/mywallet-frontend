4f38f6091614e3554ae45460ea219c0442bddce3
Lucas Passeri <lucaspasseri@poli.ufrj.br>
2021-08-14 00:35:29 -0300

    Prepare to deploy.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import ReactDOM from 'react-dom';
-import App from '../src/components/App/App';
+import ReactDOM from "react-dom";
+import React from "react";
+import App from "../src/components/App/App";
 
 ReactDOM.render(<App />, document.querySelector(".root"));
